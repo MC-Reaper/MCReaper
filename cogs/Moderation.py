7d867e5bc46b41ae8f05e0b307eb24a25e0f912b
SirDoz <dozdguide@gmail.com>
2021-02-25 20:31:36 -0400

    addrole: nvm

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -754,29 +754,6 @@ class Moderation(commands.Cog):
                 em = discord.Embed(title=title_msg, description=description_msg, colour=RandomColour())
                 em.set_footer(text=f'Embed created by {ctx.message.author}')
                 await ctx.send(content=None, embed=em)
-
-    @commands.command()
-    @has_permissions(manage_roles=True)
-    async def addrole(self, ctx, user: discord.Member, roleName):
-        """Gives user a role"""
-
-        for a in roleName:
-            if (a.isnumeric()) == True:
-                try:
-                    role = discord.utils.get(ctx.guild.roles, id=roleName)
-                except:
-                    pass
-            else:
-                try:
-                    role = discord.utils.get(ctx.guild.roles, name=roleName)
-                except:
-                    return await ctx.message.reply(f"{ctx.author.mention}, {roleName} does not exist!", mention_author=True)
-
-        if not role:
-            await ctx.send(f'`{roleName}` does not exist!')
-        else:
-            await user.add_roles(role)
-            await ctx.send(f"Assigned role: {roleName} to {user}!")
 # ---------------------------------------------------------------------------
 def setup(bot):
     bot.add_cog(Moderation(bot))
