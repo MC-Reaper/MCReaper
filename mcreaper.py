38ab6c59266d9edf098926298cd905b22295cf2e
SirDoz <dozdguide@gmail.com>
2021-05-11 12:24:46 -0400

    Heroku: P Y T H O N 3 9 5

--- mcreaper.py
+++ mcreaper.py
@@ -28,7 +28,7 @@ token = config.get("bot_token") # bot_token in config.json.
 mongosrv = config.get("mongosrv") # Add your mongosrv link in config.json.
 BOT_OWNER_ID = int(config.get("bot_owner_id")) # Add your userid in config.json.
 # --------------------------------------------------------------------------
-BOT_VERSION = f'Python: v{python_version()} | Discord.py: v{discord.__version__} | Bot: v0.17B'
+BOT_VERSION = f'Python: v{python_version()} | Discord.py: v{discord.__version__} | Bot: v0.18'
 DOZ_DISCORD = 'Doz#1040'
 # ---------------------------------------------------------------------------
 HQ_SERVER_INVITE = config.get("server_invite")
