7aa833ea745269c5918cd3aba295c5b42b66ac60
SirDoz <dozdguide@gmail.com>
2021-07-22 20:42:54 -0400

    le fishe au chocolat

--- mcreaper.py
+++ mcreaper.py
@@ -28,15 +28,15 @@ token = config.get("bot_token") # bot_token in config.json.
 mongosrv = config.get("mongosrv") # Add your mongosrv link in config.json.
 BOT_OWNER_ID = int(config.get("bot_owner_id")) # Add your userid in config.json.
 # --------------------------------------------------------------------------
-BOT_VERSION = f'Python: v{python_version()} | Discord.py: v{discord.__version__} | Bot: v0.18AE'
+BOT_VERSION = f'Python: v{python_version()} | Discord.py: v{discord.__version__} | Bot: v0.2'
 DOZ_DISCORD = 'Doz#1040'
 # ---------------------------------------------------------------------------
 HQ_SERVER_INVITE = config.get("server_invite")
 BAN_GIF = config.get("ban_gif")
 NUKE_GIF = config.get("nuke_gif")
 NUKE_LAUNCH_GIF = config.get("nuke_launch_gif")
-CHANGELOG_MESSAGE = "Fixed `local variable 'possible_responses_ping' referenced before assignment`\n`banmentions`\nafk engrish"
-CHANGELOG_DATE = '11/05/2021'
+CHANGELOG_MESSAGE = "Lib update"
+CHANGELOG_DATE = '22/07/2021'
 # ! DO NOT EDIT !
 # ---------------------------------------------------------------------------
 # MongoDB Configuration
