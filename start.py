ef97a93bf08a56b2f70b53b05dc8e8b37b98d706
Doz <dozdguide@gmail.com>
2021-10-26 18:04:11 -0400

    Stop autores script when requirements are not met

--- start.py
+++ start.py
@@ -3,6 +3,18 @@
 
 import subprocess, os, sys
 
+# ---------------------------------------------------------------------------
+# Modules (check if modules are installed, will come with a better implementation soon)
+try:
+    import discord, json, asyncio, random, nekos, pyfiglet, pymongo
+except ImportError:
+    print(
+    'You baka! You forgot to install the required modules in requirements.txt!',
+    '\nInstall them with pip install -r requirements.txt'
+    )
+    exit()
+# ---------------------------------------------------------------------------
+
 def start():          
     while True:
         if os.path.isfile('quit.txt'):
